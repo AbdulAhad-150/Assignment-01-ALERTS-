13093aae6e0c1e66862a874264bfcf2be3d2fcb1
AbdulAhad-150 <abdulahad150@gmail.com>
2023-09-29 04:31:49 -0700

    js_alert work

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("Assalam o alikum \r\r\rQuestion 1");
+
+alert("Error! Please enter a valid Password\r\r\rQuestion 2");
+
+alert("Welcome to JS Land \r Happy Coding\r\r\rQuestion 3");
+
+alert("Welcome to JS Land...\r\r\rQuestion 4");
+
+alert(
+  "Happy Coding!\rPrevent this page from creating additional dialogs\r\r\rQuestion 5"
+);
+
+alert(
+  "Practice placement of <script></script> element in following sections of your project in exercise 6:\ra. Head\rb. Body (before your page’s HTML)\r c. Body (inside your page’s HTML) \rd. Body (after your page’s HTML)\r ANSWER \r\r d "
+);
